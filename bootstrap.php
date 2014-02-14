0c0761b33b3e67076d7f595b341e595781ee76e7
André R <ar@ez.no>
2014-02-14 14:04:56 +0100

    Change boostrap.php to not have to copy config.php when running stock tests

--- bootstrap.php
+++ bootstrap.php
@@ -13,9 +13,13 @@ use eZ\Publish\Core\MVC\Legacy\Kernel as LegacyKernel;
 use eZ\Publish\Core\MVC\Legacy\Kernel\CLIHandler as LegacyKernelCLI;
 
 // Get global config.php settings
-if ( !( $settings = include ( __DIR__ . '/config.php' ) ) )
+if ( !file_exists( __DIR__ . '/config.php' ) && !( $settings = include ( __DIR__ . '/config.php-DEVELOPMENT' ) ) )
 {
-    throw new \RuntimeException( 'Could not find config.php, please copy config.php-DEVELOPMENT to config.php & customize to your needs!' );
+    throw new \RuntimeException( 'Could not read config.php-DEVELOPMENT, please copy stock config.php-DEVELOPMENT to config.php & customize to your needs!' );
+}
+else if ( !( $settings = include ( __DIR__ . '/config.php' ) ) )
+{
+    throw new \RuntimeException( 'Could not read config.php, please copy config.php-DEVELOPMENT to config.php & customize to your needs!' );
 }
 
 // Setup class loader, detect ezpublish-community repo context and use vendor files from there if that is the case
